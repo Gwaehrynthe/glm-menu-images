632aacb32d17af4173b3e517f92b7efd8a9e3e7c
Anthony Talarico <astalarico@gmail.com>
2017-03-27 17:34:36 -0400

    adding dimesions and crop capability

--- menu_walker_edit.php
+++ menu_walker_edit.php
@@ -5,24 +5,31 @@ class GLM_Nav_Menu_Item_Custom_Fields {
     
 	static $options = array(
 		'display_template' => '
-            <label style="display: inline-block;margin-top: 25px;" for="check-image-{id}">
+            <label class="check-image-label" for="check-image-{id}">
                 <input id="check-image-{id}" class="field-check-image" type="checkbox">
                 Menu Image
             </label>
-            <div class="image-preview-wrapper" style="display: none;">
+            <div class="image-preview-wrapper">
                 <h4 class="menu-image-header"> Menu Item Image </h4>
-                <img id="image-preview-{id}" class="image-preview" src="{value}" width="100" height="100" style="max-height: 100px; width: 100px;display: block;margin-bottom: 5px;">
-                <select>
+                <img id="image-preview-{id}" class="image-preview" src="{value}">
+                <select value="{crop}" name="image-crop-{id}" class="crop-options">
+                    <option disabled selected value="">Crop</option>
                     <option value="center bottom">Top</option>
                     <option value="center top">Bottom</option>
                     <option value="center center">Top and bottom</option>
                 </select>
+                <span class="crop-value">{crop}</span>
+                <div class="image-dimensions">
+                    <label for="image-width-{id}"> Width </label>
+                    <input value="{width}" name="image-width-{id}" id="image-width-{id}" type="text" class="image-width">
+                    <label for="image-height-{id}"> Height </label>
+                    <input value="{height}" name="image-height-{id}" id="image-height-{id}" type="text" class="image-height">
+                </div>
                 <input id="edit-menu-item-{name}-{id}" class="image-url"type="hidden" name="menu-item-{name}[{id}]" >
                 <input id="upload-button-{id}" type="button" class="button upload-button" value="Upload Image" />
 
                 <input style="display: none;" class="delete-image" type="button" value="Delete Image" />
             </div>
-            
 		',
 	);
     
@@ -66,7 +73,11 @@ class GLM_Nav_Menu_Item_Custom_Fields {
 		$new_fields = '';
 		foreach( self::$options['fields'] as $field ) {
 			$field['value'] = str_replace(' ', '/', get_post_meta($item->ID, self::get_menu_item_postmeta_key($field['name']), true) );
-			$field['id'] = $item->ID;
+			$field['id']    = $item->ID;
+            $field['width'] = get_post_meta($item->ID, '_menu_image_width', true);
+            $field['height'] = get_post_meta($item->ID, '_menu_image_height', true);
+            $field['crop'] = get_post_meta($item->ID, '_menu_image_crop', true);
+            echo $field['width'] . " ZZ";
 			$new_fields .= str_replace(
 				array_map(function($key){ return '{' . $key . '}'; }, array_keys($field)),
 				array_values(array_map('esc_attr', $field)),
@@ -83,13 +94,20 @@ class GLM_Nav_Menu_Item_Custom_Fields {
 		}
 
 		foreach( self::$options['fields'] as $field_schema ) {
+            
 			$form_field_name = 'menu-item-' . $field_schema['name'];
 			// @todo FALSE should always be used as the default $value, otherwise we wouldn't be able to clear checkboxes
 			if (isset($_POST[$form_field_name][$post_id])) {
+                $crop = $_POST['image-crop-' . $post_id];
+                $width = $_POST['image-width-' . $post_id];
+                $height = $_POST['image-height-' . $post_id];
 				$key = self::get_menu_item_postmeta_key($field_schema['name']);
 				$value = $_POST[$form_field_name][$post_id];
-                echo $value;
+                
 				update_post_meta($post_id, $key, $value);
+                update_post_meta($post_id,'_menu_image_crop', $crop);
+                update_post_meta($post_id,'_menu_image_width', $width);
+                update_post_meta($post_id,'_menu_image_height', $height);
 			}
 		}
 	}
@@ -118,7 +136,6 @@ function glm_menu_item_additional_fields( $fields ) {
 		'container_class'   => 'menu-image',
 		'input_type'        => 'hidden',
 	);
-	
 	return $fields;
 }
 ?>
