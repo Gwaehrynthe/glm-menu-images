1ebe142f9b15bf266b61d2ee1a86776911e656d3
Anthony Talarico <astalarico@gmail.com>
2017-03-28 20:33:58 -0400

    adding nav menu collection

--- settings/settings-form.php
+++ settings/settings-form.php
@@ -1,3 +1,19 @@
+<?php 
+    $nav_menus = get_registered_nav_menus(); 
+
+    foreach($nav_menus as $key=>$value){
+        $menu_obj = get_term( $key, 'nav_menu' );
+        echo $menu_obj;
+    }
+$theme_locations = get_nav_menu_locations();
+
+$menu_obj = get_term( $theme_locations[$theme_location], 'nav_menu' );
+
+$menu_name = $menu_obj->name;
+echo $menu_name;
+?>
+
+
 <form method="POST" action="">
     <input name="default-image-width" type="number">
     <input name="default-image-height" type="number">
